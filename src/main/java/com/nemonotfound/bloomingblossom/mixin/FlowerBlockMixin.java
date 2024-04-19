babdc510ca65f84c1e532dc031fdaa2b90b10a32
NemoNotFound <unconfigured@null.spigotmc.org>
2024-04-19 18:49:14 +0400

    Update 1.2.0
    (cherry picked from commit ce576893f05e5669ee79d3979bf032fdc15d7a7e)

---
+++ src/main/java/com/nemonotfound/bloomingblossom/mixin/FlowerBlockMixin.java
@@ -0,0 +1,31 @@
+package com.nemonotfound.bloomingblossom.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(FlowerBlock.class)
+public class FlowerBlockMixin implements Fertilizable {
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        FlowerBlock.dropStack(world, pos, new ItemStack(state.getBlock()));
+    }
+}
---
+++ src/main/java/com/nemonotfound/bloomingblossom/mixin/WitherRoseBlockMixin.java
@@ -0,0 +1,31 @@
+package com.nemonotfound.bloomingblossom.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.WitherRoseBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(WitherRoseBlock.class)
+public class WitherRoseBlockMixin implements Fertilizable {
+
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return false;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+
+    }
+}
